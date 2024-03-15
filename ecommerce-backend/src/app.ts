ffcf9030cc65e05660e65713198da669699e3c4d
Piyush Gupta <guptapiyush315@gmail.com>
2024-03-15 18:37:16 +0530

    Enable dashboard route in app.js and app.ts

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -14,7 +14,7 @@ import userRoute from "./routes/user.js";
 import productRoute from "./routes/products.js";
 import orderRoute from "./routes/order.js";
 import paymentRoute from "./routes/payment.js";
-// import dashboardRoute from "./routes/stats.js";
+import dashboardRoute from "./routes/stats.js";
 
 
 config({
@@ -49,7 +49,7 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/order", orderRoute);
 app.use("/api/v1/payment", paymentRoute);
-// app.use("/api/v1/dashboard", dashboardRoute);
+app.use("/api/v1/dashboard", dashboardRoute);
 
 app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
